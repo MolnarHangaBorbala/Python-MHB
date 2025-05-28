4717c5c7fc4b77b0926146b2651423ecde3e7d79
MolnarHangaBorbala <molnarhanga4@gmail.com>
2025-05-28 14:15:50 +0200

    Add files via upload

--- kutya_Class.py
+++ kutya_Class.py
@@ -21,7 +21,9 @@ with open("kutya.txt", encoding="utf-8") as file:
         kutyalst.append(k1)
 
 print(kutyalst)
-"""
+print("---------------------------------------------------------")
+
+
 kutyalst=[]
 
 class Kutya:
@@ -44,4 +46,60 @@ for i in file:
     kutyalst.append(k1)
 
 print(kutyalst)
+print("---------------------------------------------------------")
+"""
+
+kutyalst=[]
+
+class Kutya:
+    def __init__(self,fajta,nev,kor):
+        self.fajta=fajta
+        self.nev=nev
+        self.kor=kor
+
+    def elet(self):
+        korr=self*7
+        return korr
+
+    def __repr__(self):
+        return f"A kutya fajtája {self.fajta}, neve: {self.nev}, kora: {self.kor}"
+
+file = open("kutya.txt", encoding="utf-8")
+for i in file:
+    i=i.strip().split()
+    k1=Kutya(*i)
+    kutyalst.append(k1)
+
+print(kutyalst)
+print("---------------------------------------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
