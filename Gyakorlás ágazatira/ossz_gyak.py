43d730a9d6be5736783b26d15964a7beb3c243b4
MolnarHangaBorbala <molnarhanga4@gmail.com>
2025-06-12 14:07:28 +0200

    Add files via upload

---
+++ Gyakorlás ágazatira/ossz_gyak.py
@@ -0,0 +1,43 @@
+#kocka.py
+#A két játékos egymás után dob egy kockával.
+
+#A dobások eredményei 1 és 6 közötti egész számok.
+
+#A program jelenítse meg mindkét játékos dobását.
+
+#A program írja ki, hogy ki nyert, vagy ha döntetlen lett az eredmény.
+
+
+print("----------------------------")
+
+#edz.py
+#A program véletlenszerűen határozza meg, hogy a gyakorlási terv hány napos legyen.
+    #Ez az érték legalább 12, legfeljebb 30 nap lehet.
+
+#Minden napra generálj egy véletlenszerű gyakorlási időt órában, amely 1 és 5 óra között van.
+    #Az értékeket egy listában tárold el, napokra bontva.
+
+#Írj saját függvényt a következő értékek kiszámítására:
+    #Legkevesebb és legtöbb napi gyakorlási idő összegzése (pl. hány nap volt 1 órás, illetve 5 órás).
+    #Az átlagos napi gyakorlási idő kiszámítása, két tizedesjegy pontossággal.
+
+#A program végén jelenítsd meg:
+    #A teljes edzésterv napjainak számát
+    #A napi gyakorlási idők listáját
+    #A legkevesebb és legtöbb napi óraszám összegét
+    #Az átlagos napi gyakorlási időt
+
+
+print("----------------------------")
+
+#jatekosok.py
+#Olvassa be a játékosok adatait a fájlból, és tárolja őket egy Jatekos osztály segítségével!
+
+#Írja ki, hogy hány játékos szerepel az állományban!
+
+#Vizsgálja meg, hogy van-e olyan játékos, akit Messinek hívnak!
+
+#Számolja meg, hányan voltak válogatott játékosok!
+
+#Kérjen be a felhasználótól egy mezszámot, és írja ki annak a játékosnak a nevét és csapatát, akinek ilyen a mezszáma!
+
